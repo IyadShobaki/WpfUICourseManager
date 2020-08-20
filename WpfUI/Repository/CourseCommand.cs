33df5a2a1f45477d97f7c43f83362d6f08f2a38d
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-20 19:56:05 -0400

    Create Application Commands

---
+++ WpfUI/Repository/CourseCommand.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.Models;
+
+namespace WpfUI.Repository
+{
+    class CourseCommand
+    {
+        private string _connectionString;
+
+        public CourseCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public IList<CourseModel> GetList()
+        {
+            List<CourseModel> courses = new List<CourseModel>();
+
+            var sql = "Course_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                courses = connection.Query<CourseModel>(sql).ToList();
+            }
+            return courses;
+        }
+
+    }
+}
---
+++ WpfUI/Repository/StudentCommand.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.Models;
+
+namespace WpfUI.Repository
+{
+    class StudentCommand
+    {
+        private string _connectionString;
+
+        public StudentCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public IList<StudentModel> GetList()
+        {
+            List<StudentModel> students = new List<StudentModel>();
+
+            var sql = "Student_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                students = connection.Query<StudentModel>(sql).ToList();
+            }
+            return students;
+        }
+    }
+}
