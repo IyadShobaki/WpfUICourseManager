9e20fc7e33eeaa876dc1c3e879d1afd164e5687f
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-20 19:25:12 -0400

    Cleanup and Create Startup
    - Install Dapper, and Caliburn packages.
    - Create the folders
    - delete the main window
    - Set up App.xaml

---
+++ README.md
@@ -0,0 +1,2 @@
+# WpfUICourseManager
+ C# and Sql udemy class - WPF(.NET Framework) Application using Dapper, Caliburn (MVVM)
---
+++ WpfUI/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfUI.ViewModels;
+
+namespace WpfUI
+{
+    class Startup : BootstrapperBase
+    {
+        public Startup()
+        {
+            Initialize();
+
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
