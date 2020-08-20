cd3b3a69c5aa191d85b662f6ad8ce0f252b0fb7c
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-20 19:29:48 -0400

    Add the Models to the Models Folder
    Added Student, Course and Enrollment models.

---
+++ WpfUI/Models/CourseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUI.Models
+{
+    class CourseModel
+    {
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ WpfUI/Models/EnrollmentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUI.Models
+{
+    class EnrollmentModel
+    {
+        public int EnrollmentId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public bool IsCommitted { get; set; }
+
+    }
+}
