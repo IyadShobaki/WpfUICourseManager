44840e5f4406953ca145d8b1cd40afb957f246aa
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-20 21:32:48 -0400

    Final Commit

--- WpfUI/ViewModels/MainViewModel.cs
+++ WpfUI/ViewModels/MainViewModel.cs
@@ -18,11 +18,16 @@ namespace WpfUI.ViewModels
 
         private string _appStatus;
         private EnrollmentModel _selectedEnrollment;
+
+        private EnrollmentCommand _enrollmentCommand;
         public MainViewModel()
         {
             SelectedEnrollment = new EnrollmentModel();
             try
             {
+                _enrollmentCommand = new EnrollmentCommand(_connectionString);
+                Enrollments.AddRange(_enrollmentCommand.GetList());
+
                 StudentCommand studentCommand = new StudentCommand(_connectionString);
                 Students.AddRange(studentCommand.GetList());
 
@@ -31,10 +36,89 @@ namespace WpfUI.ViewModels
             }
             catch (Exception ex)
             {
-                AppStatus = ex.Message;
-                NotifyOfPropertyChange(() => AppStatus);
+                UpdateAppStatus(ex.Message);
+                //AppStatus = ex.Message;
+                //NotifyOfPropertyChange(() => AppStatus);
             }
         }
+
+        public CourseModel SelectedEnrollmentCourse
+        {
+            get
+            {
+                try
+                {
+                    var courseDictionary = _courses.ToDictionary(b => b.CourseId);
+                    if (SelectedEnrollment != null && courseDictionary.ContainsKey(SelectedEnrollment.CourseId))
+                    {
+                        return courseDictionary[SelectedEnrollment.CourseId];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateAppStatus(ex.Message);
+                }
+                return null;
+            }
+            set
+            {
+                try
+                {
+                    var selectedEnrollmentCourse = value;
+                    SelectedEnrollment.CourseId = selectedEnrollmentCourse.CourseId;
+
+                    NotifyOfPropertyChange(() => SelectedEnrollment);
+                }
+                catch (Exception ex)
+                {
+                    UpdateAppStatus(ex.Message);
+                }
+            }
+        }
+
+        public StudentModel SelectedEnrollmentStudent
+        {
+            get
+            {
+                try
+                {
+                    var studentDictionary = _students.ToDictionary(b => b.StudentId);
+                    if (SelectedEnrollment != null && studentDictionary.ContainsKey(SelectedEnrollment.StudentId))
+                    {
+                        return studentDictionary[SelectedEnrollment.StudentId];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateAppStatus(ex.Message);
+                }
+                return null;
+            }
+            set
+            {
+                try
+                {
+                    var selectedEnrollmentStudent = value;
+                    SelectedEnrollment.StudentId = selectedEnrollmentStudent.StudentId;
+
+                    NotifyOfPropertyChange(() => SelectedEnrollment);
+                }
+                catch (Exception ex)
+                {
+                    UpdateAppStatus(ex.Message);
+                }
+            }
+        }
+
+        public BindableCollection<EnrollmentModel> Enrollments
+        {
+            get { return _enrollments; }
+            set
+            {
+                _enrollments = value;
+            }
+        }
+
         public BindableCollection<CourseModel> Courses
         {
             get { return _courses; }
@@ -73,7 +157,52 @@ namespace WpfUI.ViewModels
             set
             {
                 _selectedEnrollment = value;
+                NotifyOfPropertyChange(() => SelectedEnrollment);
+                NotifyOfPropertyChange(() => SelectedEnrollmentCourse);
+                NotifyOfPropertyChange(() => SelectedEnrollmentStudent);
             }
+        }
+        public void CreateNewEnrollment()
+        {
+            try
+            {
+                SelectedEnrollment = new EnrollmentModel();
+                UpdateAppStatus("New enrollment created");
+                //AppStatus = "New enrollment created";
+                //NotifyOfPropertyChange(() => AppStatus);
+            }
+            catch (Exception ex)
+            {
+                UpdateAppStatus(ex.Message);
+            }
+        }
+
+        public void SaveEnrollment()
+        {
+            try
+            {
+                var enrollmentDictionary = _enrollments.ToDictionary(p => p.EnrollmentId);
+                if (SelectedEnrollment != null)
+                {
+                    _enrollmentCommand.Upsert(SelectedEnrollment);
+                    Enrollments.Clear();
+                    Enrollments.AddRange(_enrollmentCommand.GetList());
+
+                    UpdateAppStatus("Enrollment Saved");
+                    //AppStatus = "Enrollment Saved";
+                    //NotifyOfPropertyChange(() => AppStatus);
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateAppStatus(ex.Message);
+            }
+        }
+
+        private void UpdateAppStatus(string message)
+        {
+            AppStatus = message;
+            NotifyOfPropertyChange(() => AppStatus);
         }
     }
 }
